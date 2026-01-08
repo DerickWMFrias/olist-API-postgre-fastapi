16dd8295ff148e9383d620ebce1ca3bf6d60df01
DerickWMFrias <derick.william.moraes@gmail.com>
2026-01-08 12:16:52 -0300

    Adiciona interface p/ services

--- back/interfaces/__init__.py
+++ back/interfaces/__init__.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from pydantic import BaseModel
 from fastapi import Request
 from dbconfig.base import Base
+from typing import Dict
 
 """
 Interface implementada por controladores que validam algum DTO
@@ -10,4 +11,27 @@ class InterfaceController(ABC):
     @abstractmethod
     async def validate_response(self, model: Base, DTO: BaseModel):
         """Este método DEVE ser implementado pelas subclasses"""
+        pass
+
+
+
+
+class InterfaceService(ABC):
+    @abstractmethod
+    async def get_data(self, payload: Dict):
+        pass
+
+
+    @abstractmethod
+    async def register(self, payload: BaseModel):
+        pass
+
+
+    @abstractmethod
+    async def patch(self, payload: BaseModel):
+        pass
+
+
+    @abstractmethod
+    async def delete(self, payload: BaseModel):
         pass
