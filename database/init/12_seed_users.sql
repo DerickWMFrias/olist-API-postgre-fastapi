52fad82d2d51a8d82681fb4c55855ba8c3eea821
DerickWMFrias <derick.william.moraes@gmail.com>
2026-01-13 00:18:28 -0300

    Insere campo hashed_password p/ seeding

--- database/init/12_seed_users.sql
+++ database/init/12_seed_users.sql
@@ -1,11 +1,11 @@
-INSERT INTO USERS(user_id, email, passwd, recovery_email)
+INSERT INTO USERS(user_id, email, hashed_password, recovery_email)
 VALUES('f7d6e87b-4221-4f11-9252-87611e3b5e1b', 'user.email.1@mail.com', 'placeholder-passwd', 'user.rec.1@mail.com');
 
-INSERT INTO USERS(user_id, email, passwd, recovery_email)
+INSERT INTO USERS(user_id, email, hashed_password, recovery_email)
 VALUES('9e6d8a34-2b1f-4318-8671-502a3a5f7082', 'user.email.2@mail.com', 'placeholder-passwd', 'user.rec.2@mail.com');
 
-INSERT INTO USERS(user_id, email, passwd, recovery_email)
+INSERT INTO USERS(user_id, email, hashed_password, recovery_email)
 VALUES('a32e1967-8974-4b47-9759-4560738e4112', 'user.email.3@mail.com', 'placeholder-passwd', 'user.rec.3@mail.com');
 
-INSERT INTO USERS(user_id, email, passwd, recovery_email)
+INSERT INTO USERS(user_id, email, hashed_password, recovery_email)
 VALUES('67b2d56a-12e8-4993-9c54-46702e1b854a', 'user.email.4@mail.com', 'placeholder-passwd', 'user.rec.4@mail.com');
